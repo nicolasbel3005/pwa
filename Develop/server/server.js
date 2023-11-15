7154484f69600075f467e06f5f280d9af5f1347a
Nicolas Esquibel <n.m.esquibel01@gmail.com>
2023-11-14 19:02:36 -0800

    updates

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,12 +1,38 @@
+// server.js
 const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('../client/dist'));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// Middleware
+app.use(bodyParser.json());
 
-require('./routes/htmlRoutes')(app);
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
+// API endpoint for saving content
+app.post('/api/save', (req, res) => {
+  const { content } = req.body;
+  // Implement saving content to a database or any other storage here
+  console.log('Content received on the server:', content);
+  res.json({ message: 'Content saved successfully' });
+});
+
+// API endpoint for loading content
+app.get('/api/load', (req, res) => {
+  // Implement loading content from a database or any other storage here
+  // Return the loaded content as a response
+  const loadedContent = 'Content loaded from the server';
+  res.json({ content: loadedContent });
+});
+
+// Catch-all route to serve index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
